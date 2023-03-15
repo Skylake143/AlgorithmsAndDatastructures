5c9812e6cc6aee552786125f947e0b2a6c334f06
Skylake143 <45364723+Skylake143@users.noreply.github.com>
2023-03-15 21:23:57 +0100

    [mod] Search methods implemented   -- search exhaustive search   -- search backtracking   -- search greedy backtracking

--- Assignment1/csp.py
+++ Assignment1/csp.py
@@ -25,6 +25,8 @@ class CSP:
         self.width = grid.shape[1]
         self.height = grid.shape[0]
         self.numbers = numbers
+        #Sorting the numbers first is a greedier approach than using a set of numbers, since the smallest number will be used first and thus it gets less likely to run into the sum constraint
+        self.sorted_numbers = sorted(self.numbers)
         self.groups = groups
         self.constraints = constraints
 
@@ -53,7 +55,6 @@ class CSP:
                     index = self.groups.index(group)
                     self.cell_to_groups.get(elem).append(index)
         return 
-        raise NotImplementedError()
 
 
     def satisfies_sum_constraint(self, group: typing.List[typing.Tuple[int,int]], sum_constraint: int) -> bool:
@@ -73,7 +74,6 @@ class CSP:
             return True
         else: return False
 
-        raise NotImplementedError()
 
     
     def satisfies_count_constraint(self, group: typing.List[typing.Tuple[int,int]], count_constraint: int) -> bool:
@@ -96,9 +96,7 @@ class CSP:
         multiplicities = [entries.count(number) for number in self.numbers]
         #Check for each multiplicity in multiplicities if it satisfies count constraint
         return all(multiplicity<=count_constraint for multiplicity in multiplicities)
-
-        raise NotImplementedError()
-
+    
 
     def satisfies_group_constraints(self, group_indices: typing.List[int]) -> bool:
         """
@@ -120,9 +118,6 @@ class CSP:
         return True
             
 
-        raise NotImplementedError()
-
-
     def search(self, empty_locations: typing.List[typing.Tuple[int, int]]) -> np.ndarray:
         """
         Recursive exhaustive search function. It tries to fill in the empty_locations with permissible values
@@ -134,31 +129,137 @@ class CSP:
 
         :param empty_locations: list of empty locations that still need a value from self.numbers 
         """
-        #Checks if grid is already sufficiently filled
+        #Base Case: Checks if grid is already sufficiently filled
         if len(empty_locations)==0: 
-            return self.grid
+            #Check constraints of all groups
+            if(self.satisfies_group_constraints(range(len(self.groups)))):
+                return self.grid
+            else: return None
 
-        #Sort numbers
-        sorted_numbers = sorted(self.numbers)
-
+        #Recursive case
+        #Get first empty location
         location = empty_locations[0]
         #Get which groups contain location
+        #You should just need that in a backtracking solution, since we do not check solution right in the forloop for the exhaustive search
+        #affected_indices = self.cell_to_groups.get(location)
+
+        #For each number
+        for tryout in self.numbers: 
+            #Assign number to grid
+            self.grid[location[0]][location[1]] = tryout
+            #Recursively fill next location
+            result = self.search(empty_locations[1:])
+            #return result as long as constraints are fulfilled
+            #Since we do not check for the constraints here (as we will do later in the backtracking solution) 
+            #The code will basically fill all grid positions until the base case checks if the constraints are fulfilled; Only there it decides if it is returned None or self.grid
+            if result is not None: return result
+            #In case of result being None (so constraint check in base case was not succesful) we reset the location in the grid to 0 and continue with the next number
+            self.grid[location[0]][location[1]] = 0
+
+        #This return statement is just reached in case that there is no solution for the grid; according to the function describtion None should be returned then 
+        return None
+    
+    def search_backtracking(self, empty_locations: typing.List[typing.Tuple[int, int]]) -> np.ndarray:
+        """
+        Recursive exhaustive search function. It tries to fill in the empty_locations with permissible values
+        in an attempt to find a valid solution that does not violate any of the constraints. Instead of checking all
+        possible constraints after filling in a number, it checks only the relevant group constraints using the 
+        self.cell_to_groups data structure. 
+
+        Returns None if there is no solution. Returns the filled in solution (self.grid) otherwise if a solution is found.
+
+        :param empty_locations: list of empty locations that still need a value from self.numbers 
+        """
+        #Base Case: Checks if grid is already sufficiently filled
+        if len(empty_locations)==0: 
+            #Constraints do not need to be checked again here, since we already check them in the Recursive case loop
+            return self.grid
+
+        #Recursive case:
+        #Get first empty location
+        location = empty_locations[0]
+        #Get which groups contain location to check them for constraints later on
         affected_indices = self.cell_to_groups.get(location)
 
-
-        for tryout in sorted_numbers: 
+        #For each number
+        for tryout in self.numbers: 
+            #Assign number to grid
             self.grid[location[0]][location[1]] = tryout
+            #Check if constraints are still satisfied with new tryout, if not all further recursive calls will not be called (or in other words all subtrees will never be visited)
             if(self.satisfies_group_constraints(affected_indices)):
-                if(self.search(empty_locations[1:]) is None): continue
-                return self.grid
+                #Recursively fill next location
+                result = self.search_backtracking(empty_locations[1:])
+                #If there is no result (so return None at the end is reached) for given tryout we continue with next tryout
+                if result is not None: return result
+            #In case of result being None we reset the location in the grid to 0 and continue with the next number
+            self.grid[location[0]][location[1]] = 0
         
-        self.grid[location[0]][location[1]] = 0
+        #This return statement is just reached in case that there is no solution for the grid; according to the function describtion None should be returned then 
         return None
-       
-        
-        
 
-        raise NotImplementedError()
+    def helper_get_group_complement(self, group: typing.List[typing.Tuple[int,int]], sum_constraint: int) -> bool:
+        """
+        Function that checks whether the given group satisfies the given sum constraint (group smaller or equal 
+        than sum). Returns True if the current group satisfies the constraint and False otherwise. 
+
+        :param group: The list of locations [loc1, loc2, loc3,...,locN] that specify the group. Here, every loc is 
+                      a tuple (row_idx, col_idx) of indices, specifying the row and column of the cell. 
+        :param sum_constraint: The sum_of_elements constraint specifying that the numbers in the given group must
+                               sum up to this number. This is None if there is no sum constraint for the given group. 
+        """
+
+        #Evaluates the grid values at each position (y,x) in group
+        calc_sum =[self.grid[y][x] for (y,x) in group]
+        #Calculate complement
+        complement = sum_constraint-sum(calc_sum)
+        return complement
+
+    def search_greedy_backtracking(self, empty_locations: typing.List[typing.Tuple[int, int]]) -> np.ndarray:
+        """
+        Recursive exhaustive search function. It tries to fill in the empty_locations with permissible values
+        in an attempt to find a valid solution that does not violate any of the constraints. Instead of checking all
+        possible constraints after filling in a number, it checks only the relevant group constraints using the 
+        self.cell_to_groups data structure. 
+
+        Returns None if there is no solution. Returns the filled in solution (self.grid) otherwise if a solution is found.
+
+        :param empty_locations: list of empty locations that still need a value from self.numbers 
+        """
+        #Base Case: Checks if grid is already sufficiently filled
+        if len(empty_locations)==0: 
+            #Constraints do not need to be checked again here, since we already check them in the Recursive case loop
+            return self.grid
+
+        #Recursive case:
+        #Get first empty location
+        location = empty_locations[0]
+        #Get which groups contain location to check them for constraints later on
+        affected_indices = self.cell_to_groups.get(location)
+        #Find out the maximum allowed number by calculating the complement of max_sum_constraint and the sum of already filled elements; in case that max_allowed_number<min(numbers) we effectively skip the whole code and save a lot of time; otherways we just check necessary elements
+        max_allowed_number = np.inf
+        if len(affected_indices) is not 0:
+            max_allowed_number = min([self.helper_get_group_complement(self.groups[index], self.constraints[index][0]) for index in affected_indices])
+            #Instead of using numbers as in exhaustive search and backtracking, we use a sorted list of the set numbers; this is greedier since the algorithms begins checking the lowest number and thus runs less likely in the sum constraint
+            sorted_numbers_smaller_than_max = [x for x in self.sorted_numbers if x <= max_allowed_number]
+        else: 
+            sorted_numbers_smaller_than_max = self.sorted_numbers
+
+        #For each number
+        for tryout in sorted_numbers_smaller_than_max: 
+            #Assign number to grid
+            self.grid[location[0]][location[1]] = tryout
+            #Check if constraints are still satisfied with new tryout, if not all further recursive calls will not be called (or in other words all subtrees will never be visited)
+            if(self.satisfies_group_constraints(affected_indices)):
+                #Recursively fill next location
+                result = self.search_greedy_backtracking(empty_locations[1:])
+                #If there is no result (so return None at the end is reached) for given tryout we continue with next tryout
+                if result is not None: return result
+            
+        #In case of result being None we reset the location in the grid to 0 and continue with the next number
+        #Since end of for loop is anyway just reached when None is returned we can also reset the location to 0 here to make the algorithm more greedy
+        self.grid[location[0]][location[1]] = 0
+        #This return statement is just reached in case that there is no solution for the grid; according to the function describtion None should be returned then 
+        return None
     
 
     def start_search(self):
